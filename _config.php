41e2d4659a6778036a0a13c0feb56718f76d3878
Tyler Kidd <tyler@adaircreative.com>
2012-08-08 16:45:33 -0500

    Changed BlogTree to BlogHolder

--- _config.php
+++ _config.php
@@ -1,4 +1,4 @@
 <?php
 
-if(class_exists('BlogTree') && class_exists('BlogHolder_Extension')) Object::add_extension('BlogHolder','BlogHolder_Extension');
+if(class_exists('BlogHolder') && class_exists('BlogHolder_Extension')) Object::add_extension('BlogHolder','BlogHolder_Extension');
 if(class_exists('BlogEntry') && class_exists('BlogEntry_Extension')) Object::add_extension('BlogEntry','BlogEntry_Extension');
